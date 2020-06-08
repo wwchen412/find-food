b743f2f202c54cdd152bcb42142b99315a6d8c07
william <wwchen412@gmail.com>
2020-06-08 20:36:33 +0800

    Updates

---
+++ precache-manifest.bbce1d09477796e81aba1c2cc6cdf884.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f108cf56b29aac73d49415313cab27e5",
+    "url": "/find-food/index.html"
+  },
+  {
+    "revision": "e3de219690cae45a1387",
+    "url": "/find-food/static/css/main.e601e402.chunk.css"
+  },
+  {
+    "revision": "3e98be6e0853f8ca797b",
+    "url": "/find-food/static/js/2.c69065f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/find-food/static/js/2.c69065f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3de219690cae45a1387",
+    "url": "/find-food/static/js/main.2aaf0b50.chunk.js"
+  },
+  {
+    "revision": "46086200ea5ec560737e",
+    "url": "/find-food/static/js/runtime-main.f8f6a762.js"
+  },
+  {
+    "revision": "dc881fa0d4565b6a3b2c8b593572dd02",
+    "url": "/find-food/static/media/cloud1.dc881fa0.svg"
+  },
+  {
+    "revision": "73806bd252432961d6b73c3c259b389d",
+    "url": "/find-food/static/media/cloud2.73806bd2.svg"
+  },
+  {
+    "revision": "5ba04788e8f8e95d743a067129b74e54",
+    "url": "/find-food/static/media/cloud3.5ba04788.svg"
+  }
+]);
